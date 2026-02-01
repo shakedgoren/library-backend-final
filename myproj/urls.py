c8b86d97bb66fe207b550fd35db2a0057055f460
Shaked Goren <shakedgoren@Shakeds-MacBook-Air.local>
2026-02-01 15:49:34 +0200

    add netlify web

--- myproj/urls.py
+++ myproj/urls.py
@@ -17,11 +17,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ֿapi/', include('base.urls')),
 ]
 
+if not User.objects.filter(username='admin_shaked').exists():
+    User.objects.create_superuser('admin_shaked', 'admin@example.com', '12345678')
+    print("Superuser created successfully!")
+    
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
