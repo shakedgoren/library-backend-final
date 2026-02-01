10e0e97fddedf87e7e97026a20d5d01d930b479f
Shaked Goren <shakedgoren@Shakeds-MacBook-Air.local>
2026-02-01 15:13:54 +0200

    add netlify web

--- myproj/settings.py
+++ myproj/settings.py
@@ -191,3 +191,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "library-frontend-final.netlify.app", # הכתובת של הפרונט
+]
