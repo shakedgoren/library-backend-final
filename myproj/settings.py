01e96c4b0472fb42119506b4ecc0fba58c87b18c
Shaked Goren <shakedgoren@Shakeds-MacBook-Air.local>
2026-03-05 16:39:24 +0200

    Update settings for PostgreSQL, fix API URL typo, generate data.json

--- myproj/settings.py
+++ myproj/settings.py
@@ -123,11 +123,13 @@ WSGI_APPLICATION = 'myproj.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = { 
-   'default': { 
-       'ENGINE': 'django.db.backends.sqlite3', 
-       'NAME': BASE_DIR / 'db.sqlite3', 
-   } 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
--- myproj/urls.py
+++ myproj/urls.py
@@ -22,7 +22,7 @@ from django.db.utils import OperationalError
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ֿapi/', include('base.urls')),
+    path('api/', include('base.urls')),
 ]
     
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
